2b66fea9c3d7ccd5c4c5592a2bd48e4b1f85567d
snkben <snakelonely@outlook.com>
2019-03-11 17:58:31 +0800

    modify by bug

--- Nuke/py273/OTC_convenientKits.py
+++ Nuke/py273/OTC_convenientKits.py
@@ -26,7 +26,7 @@ class OTC_convenientKits(object):
             # eard = readNodes[0]
             stuffPath = eard.knob("file").getValue()
             cam_name = re.search('cam[\w_]*(r|l)', stuffPath, re.I).group()
-            camOriant = re.search('r|l', cam_name, re.I).group()
+            camOriant = re.search('(r|l)$', cam_name, re.I).group()
             cam_name_new = re.sub(camOriant, self.dict_rl[camOriant], cam_name)
             stuffPaht_new = re.sub(cam_name, cam_name_new, stuffPath)
             stuffPaht_new_2 = stuffPaht_new + "abc.exe"

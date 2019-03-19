19d6443dfd35a831ae87c119412dcca9aa3765bc
snkben <snakelonely@outlook.com>
2019-03-19 18:42:50 +0800

    modify by bug

---
+++ maya_sixteen/Python/old/tdCheck.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = tdCheck
+__author__ = zhangben 
+__mtime__ = 2019/3/19 : 18:42
+# code is far away from bugs with the god animal protecting
+I love animals. They taste delicious.
+"""

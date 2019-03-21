f03de16bd8f45a3a561d620f7271884a4fd95c91
snkben <snakelonely@outlook.com>
2019-03-21 16:35:07 +0800

    modify by bug

---
+++ maya_sixteen/Python/OCT_lgt/zb_DupLightLink.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = zb_DupLightLink.py
+__author__ = zhangben 
+__mtime__ = 2019/3/21 : 15:56
+# code is far away from bugs with the god animal protecting
+I love animals. They taste delicious.
+"""
+import pymel.core as pm
+import json
+import os
+
+def write_lightLinks(sourceGrps):
+    ws = pm.workspace.name
+    out_file = {}
+    nmspc = sourceGrps.namespace()
+    tmpDir = os.getenv('TMP')
+    wf = os.path.abspath(os.path.join(tmpDir, '{}_lightLink.json'.format(nmspc.strip(':'))))
+    listAllObject = [ea.getParent() for ea in sourceGrps.listRelatives(ad=True, s=True, ni=True) if not ea.isIntermediate()]
+    l_l_dict = {}
+    for ea in listAllObject:
+        list_lights = pm.lightlink(q=True, object=ea)
+        l_l_dict[ea.name(stripNamespace=True)] = list_lights
+    with open(wf, 'w') as f:
+        f.write(json.dumps(l_l_dict, indent=4))
+    out_file[nmspc.strip(':')] = wf
+    return out_file
+def doLink(targGrp, srcGrpNSP):
+    tmpDir = os.getenv('TMP')
+    readFile = os.path.abspath(os.path.join(tmpDir, '{}_lightLink.json'.format(srcGrpNSP.strip(':'))))
+    readInfo = None
+    with open(readFile, 'r') as rf:
+        readInfo = json.load(rf)
+    listAllObject = [ea.getParent() for ea in targGrp.listRelatives(ad=True, s=True, ni=True) if not ea.isIntermediate()]
+    ret_dic = {'linked': [], 'failed': [], 'noLinked': []}
+    for eaObj in listAllObject:
+        getKey = eaObj.name(stripNamespace=True)
+        if getKey not in readInfo:
+            ret_dic['failed'].append("Node {} not find in source Select Group".format(eaObj.longName()))
+            continue
+        ea_l_l = readInfo[getKey]
+        if not len(ea_l_l):
+            ret_dic['noLinked'].append("WARNING!!!!!:Node {} not find in source Select Group".format(eaObj.longName()))
+            continue
+        for ealight in readInfo[getKey]:
+            pm.lightlink(object = eaObj, light=ealight)
+
+def DupLightLink():
+    sel_groups = pm.selected()
+    if len(sel_groups) !=2: pm.error("Please select 2 group: 1--source character; 2--target group")
+    readDate =  write_lightLinks(sel_groups[0])
+
+    doLink(sel_groups[1],readDate.keys()[0])
+
--- maya_sixteen/Python/OCT_proxy/OCT_ExchangeProxy_v1.py
+++ maya_sixteen/Python/OCT_proxy/OCT_ExchangeProxy_v1.py
@@ -29,9 +29,10 @@ class OCT_ExchangeProxy(object):
     def __init__(self):
         self.keywd2dir = {'AR': 'arnoldtex', 'VR': 'Vray_DL', '.ass': 'arnoldtex', '.vrmesh': 'Vray_DL', 'AR_MOD': 'scenes', 'VR_MOD': 'scenes',
                           'MOD': 'scenes'}  # via key words get proxy file store folder name
-        self.prx_file_attr = {'aiStandIn': 'dso', 'VRayMesh': 'fileName', 'AR': 'dso', 'VR': 'fileName','file':'fileTextureName'}  # each type proxy node has different attribute name
+        self.prx_file_attr = {'aiStandIn': 'dso', 'VRayMesh': 'fileName2', 'AR': 'dso', 'VR': 'fileName','file':'fileTextureName'}  # each type proxy node has different attribute name
         self.prx_type_abbr = {'vrmesh': 'VR', 'ass': 'AR', 'aiStandIn': 'AR', 'VRayMesh': 'VR'}  # various key words symbol as a acronym word
 
+        self.insType = {'VRayMesh':'mesh','aiStandIn':'aiStandIn','mesh':'mesh'}
         #下面这个字典比较复杂 每个key 就是替换的代理类型，values 是一个列表，分别用于不同的判断
         #item 1  导入对应的文件后，会有导入的节点的记录，判断节点的类型如果是 对应的节点，就要修改属性 vr代理的
         #item 2  对应节点要重设的属性名字
@@ -43,15 +44,51 @@ class OCT_ExchangeProxy(object):
         self.prx_f_ext_dic = {'AR': '.ass', 'aiStandIn': '.ass', '_VR.ma': '.vrmesh', 'VRayMesh': '.vrmesh'}
         self.prx_im_f_ext_dic = {'AR': '_AR.ass', 'aiStandIn': '_AR.ass', 'VR': '_VR.ma', 'VRayMesh': '_VR.ma', 'MOD': '_MOD.mb', 'AR_MOD': '_AR.mb',
                                  'VR_MOD': '_VR.mb'}
+        self.prx_im_f_ext_dic_vr = {'AR': '_AR.ass', 'VR': '_VR.vrmesh'}
         self.im_f_t = {'AR': 'ASS', 'VR': 'mayaAscii', 'MOD': 'mayaBinary', 'AR_MOD': 'mayaBinary', 'VR_MOD': 'mayaBinary'}
 
         self.prx_serv_dir = r"\\octvision.com\CG\Resource\Material_Library\Proxy\ProxySeed"
 
-        self.path_spl_ch = {'AR': '\\', 'VR': '/', 'aiStandIn': '\\', 'VRayMesh': '/', '.ass': '\\', '.vrmesh': '/', 'MOD': '\\', 'VR_MOD': '\\',
-                            'AR_MOD': '\\'}
+        self.path_spl_ch = {'AR': r'\\', 'VR': '/', 'aiStandIn': r'\\', 'VRayMesh': '/', '.ass': '\\', '.vrmesh': '/', 'MOD': r'\\', 'VR_MOD': r'\\',
+                            'AR_MOD': r'\\'}
         self.needCopy_ext = ['', '.ass', '.vrmesh', '.jpg']
         self.nowayEx = []
         self.cur_prj = pm.workspace.name
+
+    def ExchangeProxy(self, prx_type, trg_type, ifAll=None):
+        sel_prxs_dict = self.list_prxys_dict(self.get_prxs(prx_type, ifAll))
+        need_prxs_date = self.nessesary_prxDcit(sel_prxs_dict)
+        # src_type = 'AR'
+        # trg_type = 'VR'
+        im_objs = self.imp_all_prxs(need_prxs_date, trg_type)
+        #print im_objs
+        for ea_prx_type in sel_prxs_dict:  # pass
+            for eaPrxShp in sel_prxs_dict[ea_prx_type]:  # pass
+                if prx_type == 'VRayMesh':
+                    eaPrxShp = eaPrxShp.attr('output').listConnections(sh=True,type='mesh')[0]
+                trns_nd = eaPrxShp.getParent()
+                grp_nd = trns_nd.getParent()
+                cp_prx_trn = ""
+                if eaPrxShp.isInstanced():
+                    cp_prx = pm.instance(im_objs[ea_prx_type]['needRep'],st=True)
+                    cp_prx_trn = cp_prx[0]
+                    print ('instance object {}'.format(cp_prx))
+                else:
+                    cp_prx = pm.duplicate(im_objs[ea_prx_type]['needRep'], rr=True)
+                    cp_prx_trn = cp_prx[0]
+                    if trg_type == 'VR':
+                        get_vrmsh = im_objs[ea_prx_type]['needRep'].attr('inMesh').listConnections(type='VRayMesh',p=True)[0]
+                        get_vrmsh >> cp_prx_trn.getShape().attr('inMesh')
+                if grp_nd:
+                    try: cp_prx_trn.setParent(grp_nd)
+                    except: raise Exception("TD Check")
+                print ("rename =========ok")
+                trns_nd.translate >> cp_prx_trn.translate
+                trns_nd.rotate >> cp_prx_trn.rotate
+                trns_nd.scale >> cp_prx_trn.scale
+                pm.delete(trns_nd)
+        for ea_v in im_objs.values():
+            pm.delete(ea_v['trans'])
 
     def getInstances(self):  # get all instance shape node list
         instances = []
@@ -83,9 +120,13 @@ class OCT_ExchangeProxy(object):
             src_type = re.search('(_AR|_VR)', prx_f_bsnm).group().strip("_")
             src_file_ext = os.path.splitext(ea_prx)[-1]
             current_prx_servDir = need_prxs_date[ea_prx]
+            print current_prx_servDir
+            print self.path_spl_ch[src_file_ext]
             curr_prx_servDir_split = current_prx_servDir.split(self.path_spl_ch[src_file_ext])
+            print curr_prx_servDir_split
+            if len(curr_prx_servDir_split) == 1: curr_prx_servDir_split = current_prx_servDir.split('\\')
+            print('\n'.join(curr_prx_servDir_split))
             folder_indx = curr_prx_servDir_split.index(self.keywd2dir[src_file_ext])
-
             # 代理工程路径
             prx_proj_pth = self.path_spl_ch[src_file_ext].join(curr_prx_servDir_split[:folder_indx - 1])
             # 存放当前代理的 上层路径
@@ -96,7 +137,7 @@ class OCT_ExchangeProxy(object):
                 targ_above_folder = prx_proj_pth
             targ_prx_dir = '{}{}{}'.format(targ_above_folder, self.path_spl_ch[src_file_ext], self.keywd2dir[trg_type])
             sub_str = re.sub('_MOD', '_{}'.format(src_type), self.prx_im_f_ext_dic[trg_type])
-            targ_imf_nm = re.sub(self.prx_im_f_ext_dic[src_type], sub_str, ea_prx)
+            targ_imf_nm = re.sub(self.prx_im_f_ext_dic_vr[src_type], sub_str, ea_prx)
             # 导入的文件 需要copy的文件
             imf_pth_full = '{}{}{}'.format(targ_prx_dir, self.path_spl_ch[src_file_ext], targ_imf_nm)
             sub_str_2 = re.sub('scenes', '{}_txt'.format(prx_f_bsnm_nornder), self.keywd2dir[trg_type])
@@ -116,26 +157,43 @@ class OCT_ExchangeProxy(object):
     # import relative proxy file
     def import_Prx(self, imf_pth_full, prx_f_bsnm_nornder, trg_type, infoDcit):#导入代理并设置好对应的节点的路径属性
         # infoDcit = need_info
+        print ("BE ABLE TO IMPORT::::{}".format(imf_pth_full))
         im_ns = 'RepPrx_{}'.format(prx_f_bsnm_nornder)
+        existAistds = None
+        if trg_type == 'AR': existAistds = pm.ls(type='aiStandIn')
+        #raise Exception("TD Check")
         mc.file(imf_pth_full, i=True, type=self.im_f_t[trg_type], ra=True, ns=im_ns, pr=True, mergeNamespacesOnClash=False, options="v=0",
                 loadReferenceDepth="all")
         im_grp = pm.ls("{}*:*".format(im_ns))
+        if trg_type == 'AR':
+            im_aistd = [eachAistd for eachAistd in pm.ls(type='aiStandIn') if eachAistd not in existAistds]
+            im_grp.extend(im_aistd)
+            im_aistd_p = im_aistd[0].getParent()
+            im_grp.append(im_aistd_p)
         # cleanUp_Namespace(im_ns)
         # pm.namespaceInfo(
+        print ("======the nodes imported========\n{}".format('\n'.join([n.name() for n in im_grp])))
+        # raise Exception("TD Check")
         im_grp_dict = {}
         for ea_im in im_grp:  # ea_im = im_grp[5]
+            print ea_im.type()
+            #print self.prx_need_assign[trg_type][4]
+            #print ea_im.type() == self.prx_need_assign[trg_type][4]
             if ea_im.type() == self.prx_need_assign[trg_type][1]:
                 # print im_grp.index(ea_im)
                 modify_attr = ea_im.attr(self.prx_file_attr[trg_type])
                 current_attr_v = modify_attr.get()
                 new_attr = [ea_v[1] for ea_v in infoDcit['im'].values() if
                             re.match(unicode(re.sub(r'\\', self.path_spl_ch[trg_type], ea_v[0])), current_attr_v, re.I)]
+                #print("line 175",new_attr)
                 if not len(new_attr):
                     self.nowayEx.append(prx_f_bsnm_nornder)
                     new_attr = [infoDcit['im'][os.path.split(current_attr_v)[-1]][1]]
                 modify_attr.set(unicode(re.sub(r'\\', self.path_spl_ch[trg_type], new_attr[0])))
+                # if ea_im.type() =='VRayMesh':
                 print("{} proxy file imported, and the node :{} attribute has modified".format(self.prx_need_assign[trg_type][3], ea_im.name()))
-            elif ea_im.type() == self.prx_need_assign[trg_type][4]:
+            if ea_im.type() == self.prx_need_assign[trg_type][4]:
+                # print("OKOKOKOKOK")
                 im_grp_dict['needRep'] = ea_im
             elif ea_im.type() == 'transform':
                 im_grp_dict['trans'] = ea_im
@@ -145,10 +203,14 @@ class OCT_ExchangeProxy(object):
                 new_attr = [infoDcit['txt'][os.path.split(current_attr_v)[-1]][1]]
                 modify_attr.set(new_attr[0])
             nnm = ea_im.stripNamespace().strip()
-            self.renameIt(ea_im, "{}_".format(nnm))
+            try:
+                self.renameIt(ea_im, "{}_".format(nnm))
+            except:
+                pass
             # ea_im.rename("{}_".format(nnm))
         im_grp_dict['dags'] = im_grp
         self.cleanUp_Namespace(im_ns)
+        print im_grp_dict
         return im_grp_dict
 
     def list_prxys_dict(self, prxy_lst):  # 返回一个字典 key ：代理文件名字 value  代理文件路径
@@ -193,10 +255,15 @@ class OCT_ExchangeProxy(object):
                 need_asign_msg['other'][ea_itme] = lst_needCp['ncp'][ea_itme]
         if len(lst_needCp['cp']):
             for ea_ndcp in lst_needCp['cp']:
-                copy2Dir = lst_needCp['cp'][ea_ndcp][1]
-                if not os.path.splitext(copy2Dir)[-1] == "": copy2Dir = os.path.dirname(copy2Dir)
-                if os.path.isdir(lst_needCp['cp'][ea_ndcp][0]) and not os.path.isdir(copy2Dir): os.makedirs(copy2Dir)
+                copyFromDir = lst_needCp['cp'][ea_ndcp][0]
+                print "COPY FROM ===== {}".format(copyFromDir)
+                #if not os.path.splitext(copy2Dir)[-1] == "":
+                copy2Dir = os.path.dirname(lst_needCp['cp'][ea_ndcp][1])
+                print "COPY2 ==== {}".format(copy2Dir)
+                if not os.path.isdir(copy2Dir): os.makedirs(copy2Dir)
+                # if os.path.isdir(lst_needCp['cp'][ea_ndcp][0]) and not os.path.isdir(copy2Dir): os.makedirs(copy2Dir)
                 print("READY COPY :{}\n TO :      {}".format(lst_needCp['cp'][ea_ndcp][0],copy2Dir))
+                # raise Exception("TD Check")
                 shutil.copy2(lst_needCp['cp'][ea_ndcp][0], copy2Dir)
                 print("file copied from {} to {}".format(lst_needCp['cp'][ea_ndcp][0],lst_needCp['cp'][ea_ndcp][1]))
                 need_asign_msg['other'][ea_ndcp] = lst_needCp['cp'][ea_ndcp]
@@ -278,9 +345,12 @@ class OCT_ExchangeProxy(object):
             # else:
             #     cp_copy_fils['cp'][ea_cp] = []
         print ("============================================")
-        print ("cp :::{}".format(copy_files['cp']))
-        print ("ncp :::{}".format(copy_files['ncp']))
-        print ("tmp':::{}".format(copy_files['tmp']))
+        for ea in copy_files['cp'].values():
+            print ("cp :::{}".format(ea))
+        for ea in copy_files['ncp'].values():
+            print ("cp :::{}".format(ea))
+        # print ("ncp :::{}".format(copy_files['ncp'].values()))
+        # print ("tmp':::{}".format(copy_files['tmp'].values()))
         print("<<<<<<<End parse {:05d}<<<<<<<<<".format(st))
         return copy_files
 
@@ -303,10 +373,11 @@ class OCT_ExchangeProxy(object):
         else:
             lst_prxy = pm.ls(type=proxyType)
             list_inst = self.getInstances()
-            all_need_prxy.extend([ea for ea in list_inst if ea.type() == proxyType])
+            all_need_prxy.extend([ea for ea in list_inst if ea.type() == self.insType[proxyType]])
             print(len(all_need_prxy))
             all_need_prxy.extend([ea for ea in lst_prxy if ea not in all_need_prxy])
             print(len(all_need_prxy))
+
         return all_need_prxy
 
 
@@ -318,10 +389,7 @@ class OCT_ExchangeProxy(object):
             cur_num +=1
             self.renameIt(obj,newNm,cur_num)
         else:
-            obj.rename(newNm)
-
-
-
+            obj.rename(newNm,ignoreShape=True)
 
     def nessesary_prxDcit(self,prx_dict):  # proxy name and proxy file on server path
         prxy_infor_dict = {}
@@ -352,30 +420,6 @@ class OCT_ExchangeProxy(object):
                     prxy_dict[prx_nm] = prx_dir_serv
         return prxy_dict
 
-    def ExchangeProxy(self, prx_type, trg_type, ifAll=None):
-        sel_prxs_dict = self.list_prxys_dict(self.get_prxs(prx_type, ifAll))
-        need_prxs_date = self.nessesary_prxDcit(sel_prxs_dict)
-        # src_type = 'AR'
-        # trg_type = 'VR'
-        im_objs = self.imp_all_prxs(need_prxs_date, trg_type)
-        for ea_prx_type in sel_prxs_dict:  # pass
-            for eaPrxShp in sel_prxs_dict[ea_prx_type]:  # pass
-                trns_nd = eaPrxShp.getParent()
-                grp_nd = trns_nd.getParent()
-                cp_prx_trn = ""
-                if eaPrxShp.isInstanced():
-                    cp_prx = pm.instance(im_objs[ea_prx_type]['needRep'],st=True)
-                    cp_prx_trn = cp_prx[0]
-                    print ('instance object {}'.format(cp_prx))
-                else:
-                    cp_prx = pm.duplicate(im_objs[ea_prx_type]['needRep'], rr=True)
-                    cp_prx_trn = cp_prx[0]
-                if grp_nd:
-                    try: cp_prx_trn.setParent(grp_nd)
-                    except: raise Exception("TD Check")
-                print ("rename =========ok")
-                trns_nd.translate >> cp_prx_trn.translate
-                pm.delete(trns_nd)
 
     # ========================= update json file ,the file record all proxy information date ===================================
     def parse_prxyDate(self, prx_serv_dir):  # find all proxy files on server

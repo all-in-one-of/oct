fcbed2ef9626f403454d21e0ecf43d13e3611a20
kryfo <superdkk@163.com>
2019-04-18 11:24:32 +0800

    更新abc输出 json信息 包括目标物体（数组类型）、abc文件路径（ABCfile_sub为最终路径）、ref的相关信息等。

--- maya_sixteen/Python/OCT_Pipeline/scripts/ABC_Pipeline/k_ABC_proedure.py
+++ maya_sixteen/Python/OCT_Pipeline/scripts/ABC_Pipeline/k_ABC_proedure.py
@@ -4,6 +4,7 @@ import json
 import os
 import maya.mel as mm
 import re
+import pymel.core as pm
 
 class k_ABC_procedure():
 	def __init__(self):
@@ -44,7 +45,7 @@ class k_ABC_procedure():
 
 		return (kresult)
 
-	def k_getTargetInfo(self):
+	def k_getTargetInfo3(self):
 
 		# 获取需要输出abc目标的名字
 		k_targetObject = '|CDBWG_ch004001XiShi_h_ms_anim1:allAnim|CDBWG_ch004001XiShi_h_ms_anim1:Geo'
@@ -69,47 +70,120 @@ class k_ABC_procedure():
 
 		return (kresult)
 
+	def k_getTargetInfo(self,mode='GeoGroup'):
+		kresult=[]
+		DHgroup = '|DH'
+		scenesPath = os.path.normpath(os.path.split(cc.file(q=1, sn=1))[0])
+		#导出物体的名字规则
+		k_re = re.compile(r'.*:\w*Geo$|.*:\w*Geometry$')
+		abc_re = (r'\||:')
+
+		if mode == 'GeoGroup':
+			if cc.objExists(DHgroup):
+				try:
+					eachTarGroups = cc.listRelatives(DHgroup,c=1,f=1)
+					#获取GEO组 list
+					# eachTars_GeoGroup = [c for i in eachTarGroups for c in cc.listRelatives(i,c=1) if 'Geo' in c]
+					for eachTarGroup in eachTarGroups:
+						for TarGeoGroups in cc.listRelatives(eachTarGroup,c=1,f=1):
+							if k_re.search(TarGeoGroups):
+
+								TarGeoGroup = cc.ls(TarGeoGroups,sn=1)
+
+
+								dictElement = {'targetObject': TarGeoGroup}
+								kresult.append(dictElement)
+
+								dictElement.update({'tarGroupName': eachTarGroup})
+								dictElement.update({'targetObject_ln': TarGeoGroups})
+
+								ABCfile = os.path.join(scenesPath,TarGeoGroup[0])
+								ABCfile = ABCfile+'.abc'
+								#修改ABC文件名
+								TarGeoGroup_sub = re.sub(abc_re,'_',TarGeoGroup[0])
+								ABCfile_sub = os.path.join(scenesPath, TarGeoGroup_sub)
+								ABCfile_sub = ABCfile_sub + '.abc'
+
+								dictElement.update({'scenesPath': scenesPath})
+								dictElement.update({'ABCfile': ABCfile})
+								dictElement.update({'ABCfile_sub': ABCfile_sub})
+
+								ABCNodename = TarGeoGroup_sub + '_AlembicNode'
+								dictElement.update({'ABCNodename': ABCNodename})
+
+								# 添加reference信息
+								referenceRN = cc.referenceQuery(TarGeoGroup, referenceNode=1)
+								referencePath = cc.referenceQuery(referenceRN, un=1, wcn=1, filename=1)
+								referenceNamespace = cc.referenceQuery(referenceRN, namespace=1)
+
+								dictElement.update({'referenceRN': referenceRN})
+								dictElement.update({'referencePath': referencePath})
+								dictElement.update({'referenceNamespace': referenceNamespace})
+
+				except Exception as e:
+					print (e)
+			else:
+				cc.error('Cannot found animation group')
+
+
+			#print kresult
+
+			#导出数据到json
+			kresult_json = json.dumps(kresult)
+
+			k_abcjson = os.path.normpath(os.path.splitext(cc.file(q=1, sn=1))[0]+'.json')
+
+			file = open(k_abcjson, 'w')
+			file.write(kresult_json)
+			file.close()
+
+		return (kresult)
+
+	def k_getJsonInfo(self):
+		k_abcjson = os.path.normpath(os.path.splitext(cc.file(q=1, sn=1))[0] + '.json')
+
+		TargetInfo = json.loads(open(k_abcjson).read())
+		return (TargetInfo)
+
+
 	def k_expABCInfo(self,startframe,endframe,targetObject,abcFilename,pyCommond=''):
 		"""Maya ABC输出参数"""
+		str_root = ""
+		for i in targetObject:
+			str_root += " -root {0}".format(i)
+
 		k_jobArgs = "-frameRange " \
 						 + str(startframe) \
 						 + " " \
 						 + str(endframe) \
-						 + " -uvWrite -wholeFrameGeo -attr k_centerpivot -worldSpace -pythonPerFrameCallback "\
+						 + " -uv -wfg -ro -wv -attr k_centerpivot -ws -pythonPerFrameCallback "\
 						 + str(pyCommond)\
-					   	 + " -root " \
-						 + str(targetObject) \
-						 + " " \
-						 + "-file" + " " + str(abcFilename)
+						 + str(str_root) \
+						 + " -file " + str(abcFilename)
 
 		cc.AbcExport(verbose=1,j=k_jobArgs)
 
 
-	def k_setPivotCmd(self,targetObject):
-		kcmd ="\"import maya.cmds as cc;\
-		cc.xform('{0}', cpc=1);\
-		kpivot = cc.xform('{0}', q=1, ws=1, rotatePivot=1);\
-		cc.setAttr('{0}.k_centerpivotX', kpivot[0]);\
-		cc.setAttr('{0}.k_centerpivotY', kpivot[1]);\
-		cc.setAttr('{0}.k_centerpivotZ', kpivot[2]);\
-		cc.setKeyframe('{0}.k_centerpivot')\"".format(targetObject)
+	def k_pythonPerFrameCallbackCmd(self,targetObject):
+		kcmd ="\"import maya.cmds as cc\\n" \
+			  "for i in {0}:\\n" \
+			  "    if cc.objExists(i+'.k_centerpivot'):\\n" \
+			  "        cc.xform(i, cpc=1)\\n" \
+			  "        kpivot = cc.xform(i, q=1, ws=1, rotatePivot=1)\\n" \
+			  "        cc.setAttr(i+'.k_centerpivotX', kpivot[0])\\n" \
+			  "        cc.setAttr(i+'.k_centerpivotY', kpivot[1])\\n" \
+			  "        cc.setAttr(i+'.k_centerpivotZ', kpivot[2])\\n" \
+			  "        cc.setKeyframe(i+'.k_centerpivot')\\n\"".format(targetObject)
 		return (kcmd)
 
 	def excuteExpABC(self,kargs):
 		"""执行输出ABC  #k_centerpivot为写死的属性名 需要导出 两个版本的参考时加入此属性"""
 		targetObject = kargs['targetObject']
-		scenesPath = kargs['scenesPath']
 
+		abcPath = kargs['ABCfile_sub']
 		# 返回动画条的帧数范围
 		startFrame = cc.playbackOptions(q=True, minTime=True)
 		endFrame = cc.playbackOptions(q=True, maxTime=True)
-
-
-		k_abcFilename=targetObject.split('|')[-1]
-		if ':' in k_abcFilename:
-			k_abcFilename=k_abcFilename.replace(":","_")
-		abcPath = os.path.join(scenesPath,k_abcFilename)
-		abcPath = abcPath+'.abc'
 
 		#在GEO组下创建记录空间位置的属性
 		# try:
@@ -120,7 +194,7 @@ class k_ABC_procedure():
 		# 	cc.addAttr(targetObject, ln="k_centerpivotY", at='double', p='k_centerpivot')
 		# 	cc.addAttr(targetObject, ln="k_centerpivotZ", at='double', p='k_centerpivot')
 
-		self.k_expABCInfo(startFrame,endFrame,targetObject,abcPath,self.k_setPivotCmd(targetObject))
+		self.k_expABCInfo(startFrame,endFrame,targetObject,abcPath,self.k_pythonPerFrameCallbackCmd(targetObject))
 
 	def changeRefer(self):
 		"""换参考，将无材质带绑定的参考换成 有材质无绑定的参考"""
@@ -132,18 +206,22 @@ class k_ABC_procedure():
 				print (unresolveRef)
 				RNRef = cc.referenceQuery(resolveRef, referenceNode=1)
 				print (RNRef)
-				rep_unresolveRef=unresolveRef.replace('msAnim','msrender')
+				rep_unresolveRef=unresolveRef.replace('ms_anim','ms_render')
 				cc.file(rep_unresolveRef, loadReference=RNRef)
 
 	def excuteImpABC(self, kargs):
 		"""导入ABC缓存"""
-		ABCfile = kargs['ABCfile']
+		ABCfile = kargs['ABCfile_sub']
+		ABCNodename = kargs['ABCNodename']
 
 		if cc.objExists('k_tempGroup'):
 			cc.delete('k_tempGroup')
 			cc.createNode('transform', n='k_tempGroup')
 		else:
 			cc.createNode('transform', n='k_tempGroup')
+
+		if cc.objExists(ABCNodename):
+			cc.delete(ABCNodename)
 
 		cc.AbcImport(ABCfile, mode='import', reparent='k_tempGroup')
 
@@ -179,51 +257,51 @@ class k_ABC_procedure():
 
 	def connectABC(self,kargs):
 		"""ABC缓存连接到 有材质无绑定的mesh上（仅限mesh）"""
-		ABCfile = kargs['ABCfile']
-		topGroupName = kargs['topGroupName']
+		topGroupName = kargs['tarGroupName']
 
-		# CDBWG_ch004001XiShi_h_ms_anim_Geo_AlembicNode
-		ABCNodename = os.path.splitext(os.path.basename(ABCfile))[0] + '_AlembicNode'
-		# print(ABCNodename)
+		#ABCNodename = os.path.splitext(os.path.basename(ABCfile))[0] + '_AlembicNode'
+		ABCNodename = kargs['ABCNodename']
 
 		list_abcShapes = cc.listConnections(ABCNodename, s=0, sh=1, type='mesh')
 
-		for abcShape in list_abcShapes:
-			singleAbcShapeConnect = cc.listConnections(abcShape, d=0, sh=1, c=1, p=1)
-			# print(singleAbcShapeConnect)
-			# abcshape改成meshshape的名字
-			kcode = re.compile("^k_tempGroup\|\S*")
-			if not kcode.search(singleAbcShapeConnect[0]):
-				raise Exception("had not match TopGroup!")
-			connectShape = kcode.search(singleAbcShapeConnect[0]).group()
+		if list_abcShapes:
+			for abcShape in list_abcShapes:
+				singleAbcShapeConnect = cc.listConnections(abcShape, d=0, sh=1, c=1, p=1)
 
-			ConnectShape = re.sub("^k_tempGroup", topGroupName, connectShape)
+				# abcshape改成meshshape的名字
+				kcode = re.compile("^k_tempGroup\|\S*")
+				if not kcode.search(singleAbcShapeConnect[0]):
+					raise Exception("had not match ParentGroup!")
+				connectShape = kcode.search(singleAbcShapeConnect[0]).group()
 
-			# abc缓存与mesh连接
-			try:
-				cc.connectAttr(singleAbcShapeConnect[1], ConnectShape, f=1)
-			except Exception as e:
-				print (e)
+				ConnectShape = re.sub("^k_tempGroup", topGroupName, connectShape)
+
+				# abc缓存与mesh连接
+				try:
+					cc.connectAttr(singleAbcShapeConnect[1], ConnectShape, f=1)
+				except Exception as e:
+					print (e)
 
 		list_abcTranform = cc.listConnections(ABCNodename, s=0, sh=1, scn=1, type='transform')
-		list_abcTranform = list(set(list_abcTranform))
+		if list_abcTranform:
+			list_abcTranform = list(set(list_abcTranform))
 
-		for abcTranform in list_abcTranform:
-			singleAbcTranformConnect = cc.listConnections(abcTranform, d=0, sh=1, c=1, scn=1, p=1)
+			for abcTranform in list_abcTranform:
+				singleAbcTranformConnect = cc.listConnections(abcTranform, d=0, sh=1, c=1, scn=1, p=1)
 
-			for i in range(len(singleAbcTranformConnect)):
-				if i % 2 == 1:
-					kcode = re.compile("^k_tempGroup\|\S*")
-					if not kcode.search(singleAbcTranformConnect[i - 1]):
-						raise Exception("had not match TopGroup!")
-					connectShape = kcode.search(singleAbcTranformConnect[i- 1]).group()
+				for i in range(len(singleAbcTranformConnect)):
+					if i % 2 == 1:
+						kcode = re.compile("^k_tempGroup\|\S*")
+						if not kcode.search(singleAbcTranformConnect[i - 1]):
+							raise Exception("had not match TopGroup!")
+						connectShape = kcode.search(singleAbcTranformConnect[i- 1]).group()
 
-					ConnectShape = re.sub("^k_tempGroup", topGroupName, connectShape)
+						ConnectShape = re.sub("^k_tempGroup", topGroupName, connectShape)
 
-					try:
-						cc.connectAttr(singleAbcTranformConnect[i], ConnectShape, f=1)
-					except Exception as e:
-						print (e)
+						try:
+							cc.connectAttr(singleAbcTranformConnect[i], ConnectShape, f=1)
+						except Exception as e:
+							print (e)
 
 
 		try:
@@ -239,7 +317,7 @@ class k_ABC_procedure():
 
 	def k_import_abc(self):
 		self.changeRefer()
-		kargs = self.k_getTargetInfo()
+		kargs = self.k_getJsonInfo()
 		for karg in kargs:
 			self.excuteImpABC(karg)
 			self.connectABC(karg)
@@ -254,6 +332,18 @@ class k_ABC_procedure():
 		for karg in kargs:
 			self.impanim(karg)
 
+	def Hide_NoPrimaryVisiblility(self):
+		allMesh = pm.ls(type='mesh', ni=1)
+		NoPrimaryVisiblilitys = [a for a in allMesh if not a.primaryVisibility.get()]
+
+		for i in NoPrimaryVisiblilitys:
+			#i.visibility.set(0)
+			NoPrimaryVisiblilitysTramform = pm.listRelatives(i,allParents=1)
+			NoPrimaryVisiblilitysTramform[0].visibility.set(0)
+
+
+
+
 	def tempwin(self):
 		k_class = self
 		tempwindow = cc.window()
@@ -267,9 +357,13 @@ class k_ABC_procedure():
 		tempshow = cc.showWindow(tempwindow)
 
 
+
+
 if __name__ == "__main__":
 
 	a=k_ABC_procedure()
 	a.tempwin()
+
+
 
 

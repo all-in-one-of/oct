1ee03d7f12c7653894d96440b5bf4d18f2df625e
kryfo <superdkk@163.com>
2019-04-19 18:54:13 +0800

    修改 根据目标名字导出缓存类型（abc，curve） ，新增空场景根据json信息 加载数据 并连接缓存数据。

--- maya_sixteen/Python/OCT_Pipeline/scripts/ABC_Pipeline/k_ABC_proedure.py
+++ maya_sixteen/Python/OCT_Pipeline/scripts/ABC_Pipeline/k_ABC_proedure.py
@@ -9,116 +9,108 @@ import pymel.core as pm
 class k_ABC_procedure():
 	def __init__(self):
 		pass
-
-	def k_getTargetInfo2(self):
-
-		#获取需要输出abc目标的名字
-		k_targetObject = '|CDMSS_ch001002MossV2_Hair_h_msAnim:allAnim|CDMSS_ch001002MossV2_Hair_h_msAnim:tou_new'
-		#取得abc文件路径名
-		k_sn = cc.file(q=1, sn=1)
-		scenesPath=os.path.split(k_sn)[0]
-		#ABC文件路径
-		ABCfile = r'E:\work\Themes\ABC\master\CDMSS_ch001002MossV2_Hair_h_msAnim_tou_new.abc'
-		#大组名称
-		topGroupName = 'CDMSS_ch001002MossV2_Hair_h_msAnim:allAnim'
-
-
-		#获取需要输出abc目标的名字
-		# 		# k_targetObject2 = '|CDBWG_ch004001XiShi_h_ms_anim1:allAnim|CDBWG_ch004001XiShi_h_ms_anim1:Geo'
-		# 		# #取得abc文件路径名
-		# 		# k_sn = cc.file(q=1, sn=1)
-		# 		# scenesPath2=os.path.split(k_sn)[0]
-		# 		# #ABC文件路径
-		# 		# ABCfile2 = r'E:\work\Themes\ABC\master\CDBWG_ch004001XiShi_h_ms_anim1_Geo.abc'
-		# 		# #大组名称
-		# 		# topGroupName2 = 'CDBWG_ch004001XiShi_h_ms_anim1:allAnim'
-		# 		#
-		# 		# kresult=[{'targetObject':k_targetObject,'scenesPath':scenesPath,\
-		# 		# 		 'ABCfile':ABCfile,'topGroupName':topGroupName}, \
-		# 		# 		 {'targetObject': k_targetObject2, 'scenesPath': scenesPath2, \
-		# 		# 		  'ABCfile': ABCfile2, 'topGroupName': topGroupName2}\
-		# 		# 		 ]
-
-		kresult = [{'targetObject': k_targetObject, 'scenesPath': scenesPath, \
-							 'ABCfile':ABCfile,'topGroupName':topGroupName}, \
-						 ]
-
-		return (kresult)
-
-	def k_getTargetInfo3(self):
-
-		# 获取需要输出abc目标的名字
-		k_targetObject = '|CDBWG_ch004001XiShi_h_ms_anim1:allAnim|CDBWG_ch004001XiShi_h_ms_anim1:Geo'
-		#取得abc文件路径名
-		k_sn = cc.file(q=1, sn=1)
-		scenesPath=os.path.split(k_sn)[0]
-		#ABC文件路径
-		ABCfile = r'E:\work\Themes\ABC\master\CDBWG_ch004001XiShi_h_ms_anim1_Geo.abc'
-		#大组名称
-		topGroupName = 'CDBWG_ch004001XiShi_h_ms_anim1:allAnim'
-
-
-		kresult = [{'targetObject': k_targetObject, 'scenesPath': scenesPath, \
-							 'ABCfile':ABCfile,'topGroupName':topGroupName}, \
-						 ]
-
-		return (kresult)
-
-	def k_getatomInfo(self):
-		kresult=[{'groupname':'|CDBWG_ch004001XiShi_h_ms_anim1:allAnim|CDBWG_ch004001XiShi_h_ms_anim1:master',\
-				  'atomfile':"E:/work/Themes/ABC/master/ttt.atom"}]
-
-		return (kresult)
-
-	def k_getTargetInfo(self,mode='GeoGroup'):
+		self.DHgroup = '|DH'
+		
+	def k_getTargetInfo(self,mode='Group'):
+		"""获取场景信息（目标物体、组，导出类型abc、curve，参考信息），导出到当前目录下的json文件里"""
 		kresult=[]
-		DHgroup = '|DH'
 		scenesPath = os.path.normpath(os.path.split(cc.file(q=1, sn=1))[0])
 		#导出物体的名字规则
-		k_re = re.compile(r'.*:\w*Geo$|.*:\w*Geometry$')
-		abc_re = (r'\||:')
+		k_re_ch = re.compile(r'^\w+_ch\d+.*')
+		k_re_pr = re.compile(r'^\w+_pr\d+.*')
+		k_re_geo = re.compile(r'.*:\w*Geo$|.*:\w*Geometry$')
+		k_re_cv = re.compile(r'.*:\w*master$')
+		k_re_cache = (r'\||:')
 
-		if mode == 'GeoGroup':
-			if cc.objExists(DHgroup):
+		if mode == 'Group':
+			if cc.objExists(self.DHgroup):
 				try:
-					eachTarGroups = cc.listRelatives(DHgroup,c=1,f=1)
+					eachTarGroups = cc.listRelatives(self.DHgroup,c=1,f=1)
 					#获取GEO组 list
 					# eachTars_GeoGroup = [c for i in eachTarGroups for c in cc.listRelatives(i,c=1) if 'Geo' in c]
 					for eachTarGroup in eachTarGroups:
-						for TarGeoGroups in cc.listRelatives(eachTarGroup,c=1,f=1):
-							if k_re.search(TarGeoGroups):
+						#角色ch
+						if k_re_ch.search(cc.ls(eachTarGroup,sn=1)[0]):
+							for TarGeoGroups in cc.listRelatives(eachTarGroup,c=1,f=1):
+								if k_re_geo.search(TarGeoGroups):
+									TarGeoGroup = cc.ls(TarGeoGroups,sn=1)
 
-								TarGeoGroup = cc.ls(TarGeoGroups,sn=1)
+									dictElement = {'targetObject': TarGeoGroup}
+									kresult.append(dictElement)
+
+									dictElement.update({'type': 'abc'})
+									dictElement.update({'tarGroupName': eachTarGroup})
+									dictElement.update({'targetObject_ln': TarGeoGroups})
+
+									ABCfile = os.path.join(scenesPath,TarGeoGroup[0])
+									ABCfile = ABCfile+'.abc'
+									#修改ABC文件名
+									TarGeoGroup_sub = re.sub(k_re_cache,'_',TarGeoGroup[0])
+									ABCfile_sub = os.path.join(scenesPath, TarGeoGroup_sub)
+									ABCfile_sub = ABCfile_sub + '.abc'
+
+									dictElement.update({'scenesPath': scenesPath})
+									dictElement.update({'ABCfile': ABCfile})
+									dictElement.update({'ABCfile_sub': ABCfile_sub})
+
+									ABCNodename = TarGeoGroup_sub + '_AlembicNode'
+									dictElement.update({'ABCNodename': ABCNodename})
+
+									# 添加reference信息
+									referenceRN = cc.referenceQuery(TarGeoGroup, referenceNode=1)
+									referencePath = cc.referenceQuery(referenceRN, un=1, wcn=1, filename=1)
+									referenceNamespace = cc.referenceQuery(referenceRN, namespace=1)
+
+									dictElement.update({'referenceRN': referenceRN})
+									dictElement.update({'referencePath': referencePath})
+									dictElement.update({'referenceNamespace': referenceNamespace})
 
 
-								dictElement = {'targetObject': TarGeoGroup}
-								kresult.append(dictElement)
+									referencePath_re = referencePath.replace('_anim', '_render')
+									dictElement.update({'referencePath_re': referencePath_re})
+									referenceNamespace_re = referenceNamespace.replace('_anim', '_render')
+									dictElement.update({'referenceNamespace_re': referenceNamespace_re})
 
-								dictElement.update({'tarGroupName': eachTarGroup})
-								dictElement.update({'targetObject_ln': TarGeoGroups})
+						#道具pr
+						elif k_re_pr.search(cc.ls(eachTarGroup,sn=1)[0]):
+							for TarCvGroups in cc.listRelatives(eachTarGroup, c=1, f=1):
+								if k_re_cv.search(TarCvGroups):
+									TarCvGroup = cc.ls(TarCvGroups, sn=1)
 
-								ABCfile = os.path.join(scenesPath,TarGeoGroup[0])
-								ABCfile = ABCfile+'.abc'
-								#修改ABC文件名
-								TarGeoGroup_sub = re.sub(abc_re,'_',TarGeoGroup[0])
-								ABCfile_sub = os.path.join(scenesPath, TarGeoGroup_sub)
-								ABCfile_sub = ABCfile_sub + '.abc'
+									dictElement = {'targetObject': TarCvGroup}
+									kresult.append(dictElement)
 
-								dictElement.update({'scenesPath': scenesPath})
-								dictElement.update({'ABCfile': ABCfile})
-								dictElement.update({'ABCfile_sub': ABCfile_sub})
+									dictElement.update({'type': 'curve'})
+									dictElement.update({'tarGroupName': eachTarGroup})
+									dictElement.update({'targetObject_ln': TarCvGroups})
 
-								ABCNodename = TarGeoGroup_sub + '_AlembicNode'
-								dictElement.update({'ABCNodename': ABCNodename})
+									Animfile = os.path.join(scenesPath, TarCvGroup[0])
+									Animfile = Animfile + '.anim'
+									# 修改anim文件名
+									TarCvGroup_sub = re.sub(k_re_cache, '_', TarCvGroup[0])
+									Animfile_sub = os.path.join(scenesPath, TarCvGroup_sub)
+									Animfile_sub = Animfile_sub + '.anim'
 
-								# 添加reference信息
-								referenceRN = cc.referenceQuery(TarGeoGroup, referenceNode=1)
-								referencePath = cc.referenceQuery(referenceRN, un=1, wcn=1, filename=1)
-								referenceNamespace = cc.referenceQuery(referenceRN, namespace=1)
+									dictElement.update({'scenesPath': scenesPath})
+									dictElement.update({'Animfile': Animfile})
+									dictElement.update({'Animfile_sub': Animfile_sub})
 
-								dictElement.update({'referenceRN': referenceRN})
-								dictElement.update({'referencePath': referencePath})
-								dictElement.update({'referenceNamespace': referenceNamespace})
+
+
+									# 添加reference信息
+									referenceRN = cc.referenceQuery(TarCvGroup, referenceNode=1)
+									referencePath = cc.referenceQuery(referenceRN, un=1, wcn=1, filename=1)
+									referenceNamespace = cc.referenceQuery(referenceRN, namespace=1)
+
+									dictElement.update({'referenceRN': referenceRN})
+									dictElement.update({'referencePath': referencePath})
+									dictElement.update({'referenceNamespace': referenceNamespace})
+
+
+									referencePath_re = referencePath.replace('_anim', '_render')
+									dictElement.update({'referencePath_re': referencePath_re})
+									referenceNamespace_re = referenceNamespace.replace('_anim', '_render')
+									dictElement.update({'referenceNamespace_re': referenceNamespace_re})
 
 				except Exception as e:
 					print (e)
@@ -137,17 +129,22 @@ class k_ABC_procedure():
 			file.write(kresult_json)
 			file.close()
 
+
+
 		return (kresult)
 
-	def k_getJsonInfo(self):
-		k_abcjson = os.path.normpath(os.path.splitext(cc.file(q=1, sn=1))[0] + '.json')
-
-		TargetInfo = json.loads(open(k_abcjson).read())
-		return (TargetInfo)
+	def k_getJsonInfo(self,mode ='fs'):
+		"""获取当前路径下的json文件"""
+		if mode=='fs':
+			k_abcjson = os.path.normpath(os.path.splitext(cc.file(q=1, sn=1))[0] + '.json')
+			k_abcjson = k_abcjson.replace('_fs_','_an_')
+			print (k_abcjson)
+			TargetInfo = json.loads(open(k_abcjson).read())
+			return (TargetInfo)
 
 
 	def k_expABCInfo(self,startframe,endframe,targetObject,abcFilename,pyCommond=''):
-		"""Maya ABC输出参数"""
+		"""Maya AbcExport 输出参数设置"""
 		str_root = ""
 		for i in targetObject:
 			str_root += " -root {0}".format(i)
@@ -165,6 +162,7 @@ class k_ABC_procedure():
 
 
 	def k_pythonPerFrameCallbackCmd(self,targetObject):
+		"""导出abc时 加入的python语句"""
 		kcmd ="\"import maya.cmds as cc\\n" \
 			  "for i in {0}:\\n" \
 			  "    if cc.objExists(i+'.k_centerpivot'):\\n" \
@@ -214,6 +212,7 @@ class k_ABC_procedure():
 		ABCfile = kargs['ABCfile_sub']
 		ABCNodename = kargs['ABCNodename']
 
+
 		if cc.objExists('k_tempGroup'):
 			cc.delete('k_tempGroup')
 			cc.createNode('transform', n='k_tempGroup')
@@ -225,35 +224,47 @@ class k_ABC_procedure():
 
 		cc.AbcImport(ABCfile, mode='import', reparent='k_tempGroup')
 
-	def expanim(self,kargs):
-		cc.select(kargs['groupname'], r=1, hierarchy=1)
-		atomfile = kargs['atomfile']
+	def excuteExpAnim(self,kargs):
+		#烘焙关键帧
+		animfile = kargs['Animfile_sub']
+		targetObject = kargs['targetObject']
 
-		cc.file(atomfile, force=1,\
-				options="precision=8;\
-				statics=1;baked=0;sdk=0;constraint=0;animLayers=0;selected=childrenToo;\
-				whichRange=2;hierarchy=none;controlPoints=1;useChannelBox=2;options=keys;\
-				copyKeyCmd=-animation objects \
-				-option keys -hierarchy none -controlPoints 1 ", \
-				type="atomExport", pr=1, es=1)
+		startFrame = cc.playbackOptions(q=True, minTime=True)
+		endFrame = cc.playbackOptions(q=True, maxTime=True)
 
 
-		print('export successful!')
+		targetObject_trs = []
+		targetObject_cvs = cc.ls(targetObject, dag=1, type='nurbsCurve')
+		for targetObject_cv in targetObject_cvs:
+			targetObject_tr = cc.listRelatives(targetObject_cv, p=1)
+			targetObject_trs.append(targetObject_tr[0])
+
+		targetObject_trs = list(set(targetObject_trs))
+
+		cc.bakeResults(targetObject_trs, simulation=1, t=(startFrame, endFrame))
+		#cc.bakeResults(targetObject, simulation=1, t=(startFrame, endFrame), hierarchy='below')
+
+		cc.select(targetObject)
+
+		cc.file(animfile, force=1,
+				options="precision=8;intValue=17;nodeNames=1;verboseUnits=0;whichRange=1;"
+						"options=keys;hierarchy=below;controlPoints=0;shapes=1;helpPictures=0;useChannelBox=1;"
+						"copyKeyCmd=-animation objects -option keys -hierarchy below -controlPoints 0 -shape 1",
+				type="animExport", pr=1, es=1)
+
+	def excuteImpAnim(self,kargs):
+
+		animfile = kargs['Animfile_sub']
+
+		targetObject = kargs['targetObject']
+
+		cc.select(targetObject)
+
+		cc.file(animfile,i=1,
+				options="targetTime=4;copies=1;option=replace;pictures=0;connect=0;",
+				type="animImport",iv=1, mnc=0)
+
 		cc.select(cl=1)
-
-	def impanim(self,kargs):
-		cc.select(kargs['groupname'], r=1, hierarchy=1)
-		atomfile = kargs['atomfile']
-
-		melstr = 'file -import -type "atomImport"  \
-	    -options "targetTime=3;option=insert;match=hierarchy;selected=childrenToo;" \
-	    "%s";' % (atomfile)
-
-		mm.eval(melstr)
-
-		print('import successful!')
-
-
 
 	def connectABC(self,kargs):
 		"""ABC缓存连接到 有材质无绑定的mesh上（仅限mesh）"""
@@ -309,36 +320,73 @@ class k_ABC_procedure():
 		except Exception as e:
 			print (e)
 
+	def createReference(self,kargs):
+		"""根据json信息，创建参考"""
+		k_re_DH = re.compile("^\|DH\|")
 
-	def k_export_abc(self):
+		if not cc.objExists(self.DHgroup):cc.createNode('transform', n=self.DHgroup)
+
+
+		referenceNamespace=kargs['referenceNamespace']
+		referencePath = kargs['referencePath_re']
+		tarGroupName = kargs['tarGroupName']
+
+		tarGroupName_sub = re.sub(k_re_DH, '',tarGroupName)
+
+		k_referenceResolved = cc.file(referencePath,r=1,type='mayaBinary',iv=1,gl=1,
+							  shd=('displayLayers','shadingNetworks','renderLayersByName'),mnc=0,
+							  options='v=0;',namespace=referenceNamespace)
+
+		try:
+			cc.parent(tarGroupName_sub,self.DHgroup)
+		except Exception as e:
+			print(e)
+
+		return (k_referenceResolved)
+
+	def change_referenceNamespace(self,kargs,ref):
+		"""修改参考命名空间"""
+		referenceNamespace = kargs['referenceNamespace']
+		referenceNamespace_re = kargs['referenceNamespace_re']
+
+		k_namespace = cc.referenceQuery(ref, namespace=1)
+		if k_namespace == referenceNamespace:
+			try:
+				cc.file(ref,e=1,ns=referenceNamespace_re)
+			except Exception as e:
+				print(e)
+
+
+	def k_export_cache(self):
 		kargs = self.k_getTargetInfo()
+		self.Hide_NoPrimaryVisiblility()
 		for karg in kargs:
-			self.excuteExpABC(karg)
+			if karg['type'] == 'abc':
+				self.excuteExpABC(karg)
+			if karg['type'] == 'curve':
+				self.excuteExpAnim(karg)
 
-	def k_import_abc(self):
-		self.changeRefer()
+	def k_import_cache(self):
+		#self.changeRefer()
 		kargs = self.k_getJsonInfo()
 		for karg in kargs:
-			self.excuteImpABC(karg)
-			self.connectABC(karg)
+			k_ref = self.createReference(karg)
+			if karg['type'] == 'abc':
+				self.excuteImpABC(karg)
+				self.connectABC(karg)
+			elif karg['type'] == 'curve':
+				self.excuteImpAnim(karg)
+			self.change_referenceNamespace(karg,k_ref)
 
-	def k_export_atom(self):
-		kargs = self.k_getatomInfo()
-		for karg in kargs:
-			self.expanim(karg)
-
-	def k_import_atom(self):
-		kargs = self.k_getatomInfo()
-		for karg in kargs:
-			self.impanim(karg)
 
 	def Hide_NoPrimaryVisiblility(self):
+		"""隐藏 primary Visibility状态为不勾选的物体"""
 		allMesh = pm.ls(type='mesh', ni=1)
 		NoPrimaryVisiblilitys = [a for a in allMesh if not a.primaryVisibility.get()]
 
 		for i in NoPrimaryVisiblilitys:
 			#i.visibility.set(0)
-			NoPrimaryVisiblilitysTramform = pm.listRelatives(i,allParents=1)
+			NoPrimaryVisiblilitysTramform = pm.listRelatives(i,p=1)
 			NoPrimaryVisiblilitysTramform[0].visibility.set(0)
 
 
@@ -348,12 +396,9 @@ class k_ABC_procedure():
 		k_class = self
 		tempwindow = cc.window()
 		templayout = cc.columnLayout(tempwindow, columnAttach=('both', 5), rowSpacing=10, columnWidth=250)
-		a = cc.button(label=u'保存ani版本', command="k_class=k_ABC_procedure()\nk_class.k_save_ani()")
-		b = cc.button(label=u'保存render版本', command="k_class=k_ABC_procedure()\nk_class.k_save_render()")
-		c = cc.button(label=u'导出abc缓存', command="k_class=k_ABC_procedure()\nk_class.k_export_abc()")
-		d = cc.button(label=u'导入abc缓存', command="k_class=k_ABC_procedure()\nk_class.k_import_abc()")
-		e = cc.button(label=u'导出atom缓存', command="k_class=k_ABC_procedure()\nk_class.k_export_atom()")
-		f = cc.button(label=u'导入atom缓存', command="k_class=k_ABC_procedure()\nk_class.k_import_atom()")
+		c = cc.button(label=u'导出abc缓存', command="k_class=k_ABC_procedure()\nk_class.k_export_cache()")
+		d = cc.button(label=u'导入abc缓存', command="k_class=k_ABC_procedure()\nk_class.k_import_cache()")
+
 		tempshow = cc.showWindow(tempwindow)
 
 
@@ -363,7 +408,6 @@ if __name__ == "__main__":
 
 	a=k_ABC_procedure()
 	a.tempwin()
-
 
 
 
